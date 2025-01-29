33eede65678e15e8fd9470a6cca123998699280c
Kamruddin Mansoori <KM2170@zebra.com>
2025-01-29 16:11:25 +0530

    added logs

--- src/test/java/com/kamruddin/test_app/UserServiceTest.java
+++ src/test/java/com/kamruddin/test_app/UserServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Execution(ExecutionMode.CONCURRENT)
 @SpringBootTest
 class UserServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
 
     @Autowired
     private UserProvider userProvider;
@@ -23,9 +26,10 @@ class UserServiceTest {
     @Autowired
     private UserService userService;
 
-    @ParameterizedTest(name = "{0}")
+    @ParameterizedTest(name = "Send Mail Test for user: {0}")
     @MethodSource("userProvider")
     void sendMail(User user) {
+        logger.error("User: {}", user);
         assertTrue(this.userService.sendMail(user));
     }
 
