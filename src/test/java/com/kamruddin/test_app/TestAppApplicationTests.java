0628d888c3d251e87d9135989eaa30f04968556b
Kamruddin Mansoori <KM2170@zebra.com>
2025-01-30 11:06:37 +0530

    added display name

--- src/test/java/com/kamruddin/test_app/TestAppApplicationTests.java
+++ src/test/java/com/kamruddin/test_app/TestAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kamruddin.test_app;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TestAppApplicationTests {
 
 	@Test
+	@DisplayName("Context Loads Test")
 	void contextLoads() {
 	}
 
